dcf2c61ed1001294881202422eef17c18295fb2f
surajmeruva0786 <meruva24102@iiitnr.edu.in>
2026-01-21 19:43:48 +0530

    update project setup

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TINDER Clone App</h1>
+      {/* header */}
+      {/* tinder cards */}
+      {/* buttons below cards */}
+
+      {/* chat screen - header lo chat button nokkaka osthadi  */}
+      {/* individual chat screen */}
+    </div>
+  );
+}
+
+export default App;
