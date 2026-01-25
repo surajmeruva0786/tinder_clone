6109bac78dd9650180823c02bc6620dfcddc796c
surajmeruva0786 <meruva24102@iiitnr.edu.in>
2026-01-25 23:24:23 +0530

    update Chat.js

--- tinder-clone/src/Chat.js
+++ tinder-clone/src/Chat.js
@@ -1,16 +1,8 @@
 import React from 'react'
 import './Chat.css'
-function Chat() {
+function Chat({name, message, timestamp, profilePic}) {
   return (
-    <div className="chat">
-        <div className="chat__header">
-            <img src="https://www.w3schools.com/howto/img_avatar.png" alt="" />
-            <div className="chat__headerInfo">
-                <h2>Room name</h2>
-                <p>Last seen at...</p>
-            </div>
-        </div>
-    </div>
+    <div></div>
   )
 }
 
